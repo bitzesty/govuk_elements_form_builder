ba78b49bde526981a0f2fd7090ef3865e3a5eda5
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2016-03-22 12:18:15 +0000

    Add helper method to generate activemodel error summary box
    Generate error summary markup as defined in the GOV.UK elements guide here:
    https://govuk-elements.herokuapp.com/errors/#summarise-errors
    
    TODO: maybe move out of form builder into helper module
    TODO: maybe take heading and description text from locales file

--- lib/govuk_elements_form_builder/form_builder.rb
+++ lib/govuk_elements_form_builder/form_builder.rb
@@ -26,15 +26,55 @@ module GovukElementsFormBuilder
 
     include ActionView::Helpers::TagHelper
 
-    def error_summary object
-      if object.errors.any?
-        @template.content_tag(:div, class: :errors) do
-          @template.content_tag(:ul) do
-            object.errors.full_messages.map do |message|
-              @template.content_tag(:li, message)
-            end.join('').html_safe
-          end
-        end
+    def error_summary object, heading, description
+      return if object.errors.blank?
+      error_summary_div do
+        error_summary_heading(heading) +
+        error_summary_description(description) +
+        error_summary_messages
+      end
+    end
+
+    private
+
+    def error_summary_div &block
+      @template.content_tag(:div,
+          class: 'error-summary',
+          role: 'group',
+          aria: {
+            labelledby: 'error-summary-heading'
+          },
+          tabindex: '-1') do
+        yield block
+      end
+    end
+
+    def error_summary_heading text
+      @template.content_tag :h1,
+        text,
+        id: 'error-summary-heading',
+        class: 'heading-medium error-summary-heading'
+    end
+
+    def error_summary_description text
+      @template.content_tag :p, text
+    end
+
+    def error_summary_messages
+      @template.content_tag(:ul,
+          class: 'error-summary-list') do
+        prefix = object.class.name.underscore
+
+        object.errors.keys.map do |attribute|
+          error_summary_message object, prefix, attribute
+        end.flatten.join('').html_safe
+      end
+    end
+
+    def error_summary_message object, prefix, attribute
+      messages = object.errors.full_messages_for attribute
+      messages.map do |message|
+        @template.content_tag(:li, @template.content_tag(:a, message, href: "##{prefix}_#{attribute}_error"))
       end
     end
   end
